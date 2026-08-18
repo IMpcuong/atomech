461e4ff3844640d8d71cb030d9d70cad07b26a86
IMpcuong <cuongtrinhtien128@gmail.com>
2026-08-18 11:56:09 +0700

    feat(graphic): Render the motto placing/reposing through the function f(x): x = y

--- main.cpp
+++ main.cpp
@@ -17,15 +17,28 @@ int main(void)
   InitWindow(_W, _H, game_motto.c_str());
   SetTargetFPS(60);
 
+  struct pixel_pos
+  {
+    int x;
+    int y;
+  };
+  const auto start = pixel_pos{0, 0};
+  const auto end   = pixel_pos{_W, _H};
+
   while (!WindowShouldClose())
   {
     BeginDrawing();
     {
       ClearBackground(DARKGRAY);
-      int x = _W / 2;
-      int y = _H / 2;
       int font_sz = 20;
-      DrawText("Congrats! First window!", x, y, font_sz /*fontSize=*/, LIGHTGRAY);
+      for (int x = start.x; x < end.x; x += font_sz)
+      {
+        for (int y = start.y; y < end.y; y += font_sz)
+        {
+          if (x == y)
+            DrawText("Congrats! First window!", x, y, font_sz /*fontSize=*/, LIGHTGRAY);
+        }
+      }
     }
     EndDrawing();
   }
