15851cdf0e4108c19853438e7fa7d953cafaa171
IMpcuong <cuongtrinhtien128@gmail.com>
2026-08-18 11:12:29 +0700

    init: Graphic's elemental-mechanisms adventure (x3)

--- main.cpp
+++ main.cpp
@@ -13,20 +13,20 @@ const int _H = 480;
 
 int main(void)
 {
-  std::string game_motto("IMpcuong");
+  std::string game_motto = "IMpcuong";
   InitWindow(_W, _H, game_motto.c_str());
   SetTargetFPS(60);
 
   while (!WindowShouldClose())
   {
     BeginDrawing();
-
+    {
       ClearBackground(DARKGRAY);
       int x = _W / 2;
       int y = _H / 2;
       int font_sz = 20;
       DrawText("Congrats! First window!", x, y, font_sz /*fontSize=*/, LIGHTGRAY);
-
+    }
     EndDrawing();
   }
 
