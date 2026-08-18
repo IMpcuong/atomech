2a011e2ea82499c6bf2a361127447865d35c65a1
IMpcuong <cuongtrinhtien128@gmail.com>
2026-08-18 10:31:23 +0700

    init: Graphic's elemental-mechanisms adventure

---
+++ main.cpp
@@ -0,0 +1,34 @@
+#include <string>
+
+#include <raylib.h>
+
+//
+// @Docs:
+//    + https://github.com/raysan5/raylib/wiki/Working-on-macOS
+//    + https://github.com/raysan5/raylib/blob/master/examples/core/core_basic_window.c
+//
+
+const int _W = 800;
+const int _H = 480;
+
+int main(void)
+{
+  std::string game_motto("IMpcuong");
+  InitWindow(_W, _H, game_motto.c_str());
+  SetTargetFPS(60);
+
+  while (!WindowShouldClose())
+  {
+    BeginDrawing();
+
+      ClearBackground(DARKGRAY);
+      int x = _W / 2;
+      int y = _H / 2;
+      int font_sz = 20;
+      DrawText("Congrats! First window!", x, y, font_sz /*fontSize=*/, LIGHTGRAY);
+
+    EndDrawing();
+  }
+
+  return 0;
+}
